0bc5ce723d8be87cdfee27646b129ba698f4716e
geandreac <geandre51@gmail.com>
2024-06-15 16:13:51 -0400

    start project

---
+++ src/main/java/com/fametro/gestao_de_clientes/GestaoDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.fametro.gestao_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fametro/gestao_de_clientes/GestaoDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fametro.gestao_de_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
